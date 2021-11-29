2306f0c33262b3ea3593876433cd6420f42893ea
root <root@ip-172-31-22-185.ec2.internal>
2021-11-29 09:59:11 +0000

    Debug

--- result_collector_cookbook/recipes/collect-result.rb
+++ result_collector_cookbook/recipes/collect-result.rb
@@ -1,14 +1,14 @@
 HOMEDIR= "/root/bigdata_course_cookbook"
 TARGET_HOMEDIR="/home/ec2-user"
 
-stack = search("aws_opsworks_stack").first
+stack = search("aws_opsworks_stack","self:true").first
 Chef::Log.info("********** The stack's name is '#{stack['name']}' **********")
 Chef::Log.info("********** This stack gets its cookbooks from '#{stack['custom_cookbooks_source']['url']}' **********")
 
-layer = search("aws_opsworks_layer").first
+layer = search("aws_opsworks_layer", "self:true").first
 Chef::Log.info("********** The layer's name is '#{layer['name']}' **********")
 
-instance = search("aws_opsworks_instance").first
+instance = search("aws_opsworks_instance", "self:true").first
 Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}' **********")
 Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}' **********")
 Chef::Log.info("********** This instance's public IP address is '#{instance['public_ip']}' **********")
