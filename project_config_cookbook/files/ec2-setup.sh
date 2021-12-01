26d8917b61ec638b980afb4d5e4b4952e474f942
alexshinningsun <46494614+alexshinningsun@users.noreply.github.com>
2021-12-01 11:14:36 +0800

    Update ec2-setup.sh

--- project_config_cookbook/files/ec2-setup.sh
+++ project_config_cookbook/files/ec2-setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# install p7zip
 sudo amazon-linux-extras install epel
 sudo yum install p7zip -y
 sudo cp /usr/bin/7za /usr/bin/7z
+
+
 sudo yum -y install sysbench
+
+# install ioping
+sudo yum install ioping -y
+mkdir -p /tmp/ram
+sudo mount -t tmpfs -o size=512M tmpfs /tmp/ram/
+
 exit 0
 
