0e1743a5b4ce2dbabb61d6719e38dee9eab78e25
alexshinningsun <46494614+alexshinningsun@users.noreply.github.com>
2021-12-01 12:02:54 +0800

    Update collect-result.rb

--- result_collector_cookbook/recipes/collect-result.rb
+++ result_collector_cookbook/recipes/collect-result.rb
@@ -14,7 +14,7 @@ Chef::Log.info("********** The instance's hostname is '#{my_instance['hostname']
 Chef::Log.info("********** The instance's ID is '#{my_instance['instance_id']}' **********")
 Chef::Log.info("********** This instance's public IP address is '#{my_instance['public_ip']}' **********")
 
-dir_paths = ["#{HOMEDIR}/bigdata_course_cookbook/results", "#{HOMEDIR}/bigdata_course_cookbook/results/cpu-measurement", "#{HOMEDIR}/bigdata_course_cookbook/results/memory-measurement"]
+dir_paths = ["#{HOMEDIR}/bigdata_course_cookbook/results", "#{HOMEDIR}/bigdata_course_cookbook/results/cpu-measurement", "#{HOMEDIR}/bigdata_course_cookbook/results/memory-measurement", "#{HOMEDIR}/bigdata_course_cookbook/results/ioping"]
 dir_paths.each do |eachpath|
   directory "Create a directory" do
     group "root"
@@ -36,15 +36,25 @@ file '#{HOMEDIR}/bigdata_course_cookbook/results/memory-measurement/*' do
   backup false
 end
 
+file '#{HOMEDIR}/bigdata_course_cookbook/results/ioping/*' do
+  action :delete
+  backup false
+end
+
 search("aws_opsworks_instance").each do |instance|
   if "#{instance['hostname']}" != "#{my_instance['hostname']}"
+    
     execute "Copy cpu result files from '#{instance['hostname']}' ec2 to workstation" do
       command "scp -o StrictHostKeyChecking=no -v -i #{HOMEDIR}/keys/#{key_file_name} ec2-user@#{instance['private_ip']}:#{TARGET_HOMEDIR}/ec2-testing-script/cpu-measurement/result-*  #{HOMEDIR}/bigdata_course_cookbook/results/cpu-measurement/"
     end
+    
     execute "Copy memory result files from '#{instance['hostname']}' ec2 to workstation" do
       command "scp -o StrictHostKeyChecking=no -v -i #{HOMEDIR}/keys/#{key_file_name} ec2-user@#{instance['private_ip']}:#{TARGET_HOMEDIR}/ec2-testing-script/memory-measurement/result-*  #{HOMEDIR}/bigdata_course_cookbook/results/memory-measurement/"
     end
+    
+    execute "Copy ioping result files from '#{instance['hostname']}' ec2 to workstation" do
+      command "scp -o StrictHostKeyChecking=no -v -i #{HOMEDIR}/keys/#{key_file_name} ec2-user@#{instance['private_ip']}:#{TARGET_HOMEDIR}/ec2-testing-script/ioping/result-*  #{HOMEDIR}/bigdata_course_cookbook/results/ioping/"
+    end
+    
   end
-  #Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}' **********")
-  #Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}' **********")
 end 
